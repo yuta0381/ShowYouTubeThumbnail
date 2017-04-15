3bc7c1044b1927bc9fc17eca9f5441a6afb3e45c
BuildTools <unconfigured@null.spigotmc.org>
2017-04-15 20:58:44 +0900

    Revert "ShawYouTubeThumbnail"
    This reverts commit 9e5004712fc591adde5f466b72d34288d1bb4841.

--- main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-	chrome.tabs.getSelected(null, function(tab) {
-		if (tab.url.match(/https:\/\/www\.youtube\.com\/watch\?v=[^&]+/)) {
-			document.getElementById('img').src = "http://i.ytimg.com/vi/"+tab.url.match(/https:\/\/www\.youtube\.com\/watch\?v=([^&]+)/)[1]+"/mqdefault.jpg";
-		}else{window.close();}
-	})
-}
